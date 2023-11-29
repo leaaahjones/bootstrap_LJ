26b8edf534fdeef40f1ce55b59e53188cc8f23d3
leaaahjones <leahjmail@gmail.com>
2023-11-29 15:18:45 -0600

    Update Group Boostrap.py

--- Group Boostrap.py
+++ Group Boostrap.py
@@ -9,6 +9,8 @@ Created on Wed Oct 25 14:40:30 2023
 import pandas as pd
 from plotnine import *
 import os
+import numpy as np
+
 
 os.chdir("/Users/leah/Desktop/2450 Spyder/Notes")
 dat = pd.read_csv("2017_Fuel_Economy_Data.csv")
@@ -21,12 +23,11 @@ class Boot_CI():
         self.stat = "mean"
         self.dat = data
         self.n_boot = 0 
-        self.boot_stat = None 
+        self.boot_stat = []
         self.ci_level = .95
-        
+
         
     def add_sims(self):
-        self.boot_stat = []
         n = len(self.dat)
         for i in range(self.n_boot):
             boot_sample = self.dat.sample(n, replace = True)
@@ -46,21 +47,54 @@ class Boot_CI():
     def clear_sims(self):
         self.boot_stat = []
         
-    def load_data(self,data): #QUESTION: is this right? 
+    def load_data(self,data): 
         self.dat = data
         
     def update_n_boot(self, new_n_boot):
         self.n_boot = new_n_boot
         
     def set_statistic(self, stat):
+        self.boot_stat = []
         self.stat = stat
+        
+    def plot_boot(self):
+        boot_df = pd.DataFrame({'x': self.boot_stat})
+    
+        p = (
+            ggplot(boot_df, aes(x='x')) +
+            geom_histogram()
+            )
+    
+        return p
+        
+    def conf_interval(self):
+        if len(self.boot_stat) == 0:
+            print("Boot_stat is 0. Must run sample first")
+            
+        else: 
+            alpha = float(input("Please enter confidence level percentage: "))
+            alpha = alpha/2
+            np.percentile(self.boot_stat, [alpha, 100-alpha])
+            #do we need to return?  np.percent
+            
+            
+    
 
 
         
 test = Boot_CI()
-
 test.load_data(data = dat)
+test.update_n_boot(10000)
+test.add_sims()
+test.plot_boot()
 
+
+    
+
+#######
+
+
+    
 
 
 
